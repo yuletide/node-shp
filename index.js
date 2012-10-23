d68a6ab57acef7a9c22928ece4e57aa9b5e816d8
yuletide <adyule@gmail.com>
2012-10-23 01:01:32 -0700

    tweaks to allow both synch or async

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var Dbf = require(__dirname + '/lib/dbf'),
  */
 
 
-getFeatures("demo/TM_WORLD_BORDERS_SIMPL/TM_WORLD_BORDERS_SIMPL-0.3", function(err, data){
-  console.log(JSON.stringify(data));
-});
+//getFeatures("demo/TM_WORLD_BORDERS_SIMPL/TM_WORLD_BORDERS_SIMPL-0.3", function(err, data){
+//  console.log(JSON.stringify(data));
+//});
 
 // loads and parses shapefile, returns geojson
 function getFeatures(url, callback) {
@@ -27,7 +27,11 @@ function getFeatures(url, callback) {
     this.dbfFile = new Dbf(url);
   } catch(error) {
     console.error("Error parsing shapefile");
-    return callback(error, null);
+    if (callback){
+      callback(error, null);
+    } else {
+      throw new Error(error);
+    }
   }
 
   var recordsLength = instance.shpFile.records.length;
@@ -75,98 +79,15 @@ function getFeatures(url, callback) {
 
     this.json.features.push(feature);
   }
-  callback(null, this.json);
+  if (callback){
+    callback(null, this.json);
+  }
+  return this.json;
 }
-/*
-   function getOpenLayersFeatures(url, callback) {
-   this.shpURL = url+'.shp';
-   this.dbfURL = url+'.dbf';
-   this.callback = callback;
-
-   var instance = this;
-
-// Parse into OL features
-var parseShapefile = function () {
-// we can assume that shapefile and dbf have loaded at this point, but check anyhow
-if (!(instance.dbfFile && instance.shpFile)) return;
-
-var features = [];
-
-var recsLen = instance.shpFile.records.length;
-for (var i = 0; i < recsLen; i++) {
-var record = instance.shpFile.records[i];
-var attrs = instance.dbfFile.records[i];
-
-// turn shapefile geometry into WKT
-// points are easy!
-if (instance.shpFile.header.shapeType == ShpType.SHAPE_POINT) {
-var wkt = 'POINT(' + record.shape.x + ' ' + record.shape.y + ')';
-}
-
-// lines: not too hard--
-else if (instance.shpFile.header.shapeType == ShpType.SHAPE_POLYLINE) {
-// prepopulate the first point
-var points = [];//record.shape.rings[0].x + ' ' + record.shape.rings[0].y];
-var pointsLen = record.shape.rings[0].length;
-for (var j = 0; j < pointsLen; j++) {
-points.push(record.shape.rings[0][j].x + ' ' + record.shape.rings[0][j].y);
-}
-
-var wkt = 'LINESTRING(' + points.join(', ') + ')';
-}
-
-// polygons: donuts
-else if (instance.shpFile.header.shapeType == ShpType.SHAPE_POLYGON) {
-var ringsLen = record.shape.rings.length;
-var wktOuter = [];
-for (var j = 0; j < ringsLen; j++) {
-var ring = record.shape.rings[j];
-if (ring.length < 1) continue;
-var wktInner = [];//ring.x + ' ' + ring.y];
-var ringLen = ring.length;
-for (var k = 0; k < ringLen; k++) {
-wktInner.push(ring[k].x + ' ' + ring[k].y);
-}
-wktOuter.push('(' + wktInner.join(', ') + ')');
-}
-var wkt = 'POLYGON(' + wktOuter.join(', ') + ')';
-}
-
-var the_geom = OpenLayers.Geometry.fromWKT(wkt);
-features.push(new OpenLayers.Feature.Vector(the_geom, attrs));
-}
-callback(features);
-};	
-
-var onShpFail = function() { 
-alert('failed to load ' + instance.shpURL);
-};
-var onDbfFail = function() { 
-alert('failed to load ' + instance.dbfURL);
-}
-
-var onShpComplete = function(oHTTP) {
-var binFile = oHTTP.binaryResponse;
-console.log('got data for ' + instance.shpURL + ', parsing shapefile');
-instance.shpFile = new ShpFile(binFile);
-if (instance.dbfFile) parseShapefile();
-}
-
-var onDbfComplete = function(oHTTP) {
-  var binFile = oHTTP.binaryResponse;
-  console.log('got data for ' + instance.dbfURL + ', parsing dbf file');
-  instance.dbfFile = new DbfFile(binFile);
-  if (instance.shpFile) parseShapefile();
-}  
-
-this.shpLoader = new BinaryAjax(shpURL, onShpComplete, onShpFail);
-this.dbfLoader = new BinaryAjax(dbfURL, onDbfComplete, onDbfFail);
-}*/
 
 module.exports = {
-
-
-
+  readFileSync: getFeatures,
+  readFile: getFeatures
 }
 
 // ported from https://github.com/RandomEtc/shapefile-js/blob/master/src/ol_shapefile.js
