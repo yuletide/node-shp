cdb42313251c7cbe0b78990af4cd58edcb840360
yuletide <adyule@gmail.com>
2012-10-23 01:32:33 -0700

    Two tests now passing

--- test/shp.spec.js
+++ test/shp.spec.js
@@ -7,18 +7,22 @@ var shpFile = require(__dirname + '/../index');
 describe('Shapefile Reader', function() {
   it('Can correctly convert multiPolygon shapefile to GeoJSON', function(done){
     var good_json = JSON.parse(fs.readFileSync(__dirname + '/data/multipolygon.json', "utf8"));
-    console.log(JSON.stringify(good_json));
     shpFile.readFile(__dirname + '/data/multipolygon', function(error, data){
-      console.log(JSON.stringify(data));
+      expect(data.features[0].geometry.coordinates[0][0][0]).to.equal(good_json.features[0].geometry.coordinates[0][0][0][0]);
+      expect(data.features[0].geometry.coordinates[0][0][1]).to.equal(good_json.features[0].geometry.coordinates[0][0][0][1]);
+      expect(data.features[0].geometry.coordinates[0][1][0]).to.equal(good_json.features[0].geometry.coordinates[0][0][1][0]);
+      expect(data.features[0].geometry.coordinates[0][1][1]).to.equal(good_json.features[0].geometry.coordinates[0][0][1][1]);
     });
     done();
   });
 
   it('Can correctly convert polygon shapefile to GeoJSON', function(done){
     var good_json = JSON.parse(fs.readFileSync(__dirname + '/data/polygon.json', "utf8"));
-    console.log(JSON.stringify(good_json));
     shpFile.readFile(__dirname + '/data/polygon', function(error, data){
-      console.log(JSON.stringify(data));
+      expect(data.features[0].geometry.coordinates[0][0][0]).to.equal(good_json.features[0].geometry.coordinates[0][0][0]);
+      expect(data.features[0].geometry.coordinates[0][0][1]).to.equal(good_json.features[0].geometry.coordinates[0][0][1]);
+      expect(data.features[0].geometry.coordinates[0][1][0]).to.equal(good_json.features[0].geometry.coordinates[0][1][0]);
+      expect(data.features[0].geometry.coordinates[0][1][1]).to.equal(good_json.features[0].geometry.coordinates[0][1][1]);
     });
     done();
   });
@@ -28,6 +32,7 @@ describe('Shapefile Reader', function() {
     console.log(JSON.stringify(good_json));
     shpFile.readFile(__dirname + '/data/point', function(error, data){
       console.log(JSON.stringify(data));
+      expect(data.features[0].geometry.coordinates[0][0]).to.equal(good_json.features[0].geometry.coordinates[0][0]);
     });
     done();
   });
